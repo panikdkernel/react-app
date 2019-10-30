5a21c3522493c739a98429234ab8117d2edd5368
panikdkernel <panikdkernel@gmail.com>
2019-10-31 00:13:09 +0530

    Add MenuComponent

--- src/App.js
+++ src/App.js
@@ -1,7 +1,8 @@
 import React, {Component} from 'react';
 // import logo from './logo.svg';
 import { Navbar, NavbarBrand } from 'reactstrap';
-import './App.css';
+import Menu from './components/MenuComponent';
+// import './App.css';
 
 class App extends Component {
   render() {
@@ -14,6 +15,7 @@ class App extends Component {
               </NavbarBrand>
             </div>
         </Navbar>
+        <Menu/>
       </div>
     );
   }
